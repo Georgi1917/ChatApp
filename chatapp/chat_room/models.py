59d57e8b6aa2dafa4140f5dcc7f0e566b2d8df5a
Gosho Zvqra <mamarida15@abv.bg>
2024-09-01 18:40:27 +0300

    added messages model

--- chatapp/chat_room/models.py
+++ chatapp/chat_room/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Message(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    content = models.CharField(max_length=150)
+    timestamp = models.DateTimeField(auto_now_add=True)
